9a161eb3bc95205a5b0e918e8bea91bc7f6c3e44
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2022-02-19 18:28:21 +0000

    create day/night cycle
    Signed-off-by: Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>

---
+++ Assets/Scripts/DayNight.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class DayNight : MonoBehaviour
+{
+    private const int minInDay = 1440;
+    public Transform sun;
+    public float x_speed = 5f;
+    public float y_speed = 5f;
+    public float mins = 1f;
+
+    [SerializeField] private int currentMin;
+
+    private int dayCount;
+    private float timeSinceLastMin;
+
+
+    private void Update()
+    {
+        var t = Time.deltaTime;
+        // Rotate the sun
+        sun.Rotate(new Vector3(x_speed * t, y_speed * t, 0f));
+        //if mins passed
+        if (timeSinceLastMin >= mins)
+        {
+            timeSinceLastMin = 0f;
+            MinutePassed();
+        }
+        else
+        {
+            timeSinceLastMin += t;
+        }
+    }
+
+    private void MinutePassed()
+    {
+        ++currentMin;
+        if (currentMin >= minInDay)
+        {
+            // Next day
+            ++dayCount;
+            currentMin = 0;
+            Debug.Log("NextDay!");
+        }
+
+        var min = currentMin % 60;
+        var hrs = currentMin / 60;
+
+        Debug.Log("Next minute! " + hrs + "   :   " + min);
+    }
+}
