121123a2074e6a272b1649bfd42a63deac3b3756
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2022-02-19 19:40:03 +0000

    commit
    Signed-off-by: Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>

---
+++ Assets/Scripts/AI/AI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour
+{
+    public GameObject player;
+    private NavMeshAgent navmesh;
+    
+    void Start()
+    {
+        navmesh = GetComponent<NavMeshAgent>();
+    }
+    
+    void Update()
+    {
+        navmesh.destination = player.transform.position;
+    }
+}
