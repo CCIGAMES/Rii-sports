fc97666e6e353c60e7156fafe63ccd97bd2a1178
CCIGAMES <charliephilp@outlook.com>
2022-05-08 09:13:19 +0100

    yep

---
+++ Assets/Scripts/player/cursor.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour
+{
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+    void OnMouseEnter()
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    }
+
+    void OnMouseExit()
+    {
+        Cursor.SetCursor(null, Vector2.zero, cursorMode);
+    }
+}
