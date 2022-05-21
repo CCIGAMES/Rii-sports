5bc65e39ad2a284fbc6cde8e35b8179d919cc5cc
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2022-05-21 23:54:10 +0100

    migrate to fmod
    Signed-off-by: Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>

--- Assets/Scripts/panel/MainMenu.cs
+++ Assets/Scripts/panel/MainMenu.cs
@@ -1,25 +1,39 @@
+using FMOD.Studio;
+using FMODUnity;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using STOP_MODE = FMOD.Studio.STOP_MODE;
 
 public class MainMenu : MonoBehaviour
 {
+    private EventInstance theme;
+    
+    private void Awake()
+    {
+        theme = RuntimeManager.CreateInstance("event:/theme");
+    }
+    private void Start()
+    {
+        theme.start();
+    }
+
     public void Play()
     {
-        SceneManager.LoadScene("Scenes/baseballField");
+        theme.stop(STOP_MODE.IMMEDIATE);
+        SceneManager.LoadScene("Scenes/Baseball");
     }
 
-    public void Settings()
-    {
-    }
+    //TODO: settings menu
+    //public void Settings(){}
 
-    public void Dlcs()
-    {
-    }
+    //TODO: DLC system 
+    //public void Dlcs(){}
 
     public void Quit()
     {
         Application.Quit();
     }
+
     //TODO: loading screen
     //private IEnumerator Play(){}
 }
